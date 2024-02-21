08e0d5d64c12506ede19dc0b8f27dd1ffcb6a9cd
Thomas Ellwood <taellwood@gmail.com>
2024-02-21 12:23:43 +0000

    created reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_121720) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_122256) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_21_121720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "reviews"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
