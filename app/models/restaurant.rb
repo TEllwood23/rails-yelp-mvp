d6832d6b57ce3f6c11b43fa32a3b0d7d320620a4
Thomas Ellwood <taellwood@gmail.com>
2024-02-21 13:24:16 +0000

    added reference- restaurant on reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
+  validates :name, :address, :category, presence: true
+  # validates_inclusion_of :category, :in => %w(chinese italian japanese belgian french), :allow_nil => true
 end
